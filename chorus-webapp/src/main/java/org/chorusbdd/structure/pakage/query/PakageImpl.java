3463bb3b3a39df1b290756ca35ff5320bde62b8e
Mitch Beaumont <mitchell.beaumont@gmail.com>
2014-11-03 18:12:40 +0000

    fixed validation to allow null blank package id and human names as in the case of root the root package

--- chorus-webapp/src/main/java/org/chorusbdd/structure/pakage/query/PakageImpl.java
+++ chorus-webapp/src/main/java/org/chorusbdd/structure/pakage/query/PakageImpl.java
@@ -4,17 +4,12 @@ package org.chorusbdd.structure.pakage.query;
 import org.chorusbdd.structure.feature.Feature;
 import org.chorusbdd.structure.pakage.Pakage;
 
-import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.nio.file.Files.isDirectory;
-import static java.util.Objects.requireNonNull;
-import static org.apache.commons.lang3.Validate.notBlank;
 import static org.apache.commons.lang3.Validate.notNull;
-import static org.chorusbdd.structure.StructureIO.ROOT_ID;
 
 public class PakageImpl implements Pakage {
 
@@ -24,8 +19,8 @@ public class PakageImpl implements Pakage {
     private final List<Feature> features;
 
     public PakageImpl(final String id, final String humanName, final Stream<Pakage> subpackages, final Stream<Feature> features) {
-        this.id = notBlank(id);
-        this.humanName = notBlank(humanName);
+        this.id = notNull(id);
+        this.humanName = notNull(humanName);
         this.subpackages =  notNull(subpackages).collect(Collectors.<Pakage>toList());
         this.features = notNull(features).collect(Collectors.<Feature>toList());
     }
@@ -38,12 +33,6 @@ public class PakageImpl implements Pakage {
     @Override
     public String humanName() {
         return humanName;
-        //if (ROOT_ID.equals(id)) {
-        //    return "*root-package*";
-        //}
-        //return fileName().replaceAll("\\.feature", "")
-        //        .replaceAll("\\W+", " ")
-        //        .replaceAll("([a-z\\d])([A-Z])", "$1 $2");
     }
 
     @Override
