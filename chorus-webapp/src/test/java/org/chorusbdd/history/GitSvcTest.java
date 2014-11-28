05b44e8111abf266063aab568dd78b93a7ff7a14
Mitch Beaumont <mitchell.beaumont@gmail.com>
2014-11-28 17:08:05 +0000

    Added test for log following a folder

--- chorus-webapp/src/test/java/org/chorusbdd/history/GitSvcTest.java
+++ chorus-webapp/src/test/java/org/chorusbdd/history/GitSvcTest.java
@@ -146,14 +146,13 @@ public class GitSvcTest {
         assertThat(log.get(1).comment(), is("comment: file one created"));
     }
 
-
     @Test
     public void logFollowsAllFilesInAFolder() throws IOException {
         // prepare
         createAndCommitFile("foo/FileName1", "file 1 contents", "comment: file one created", BEAR);
         createAndCommitFile("foo/FileName2", "file 2 contents", "comment: file two created", BEAR);
-        createAndCommitFile("bar/FileName3", "file 2 contents", "comment: file two created", BEAR);
-        createAndCommitFile("FileName4", "file 2 contents", "comment: file two created", BEAR);
+        createAndCommitFile("bar/FileName3", "file 3 contents", "comment: file three created", BEAR);
+        createAndCommitFile("FileName4", "file 4 contents", "comment: file four created", BEAR);
         modifyAndCommitFile("foo/FileName1", "modified file 1 contents", "comment: file one modified", BEAR);
 
         // run
